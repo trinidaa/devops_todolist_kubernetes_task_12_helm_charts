079d380b0cc074c747aeeea17bf7bbf69dae44ce
Viacheslav Kaliak <trinidaa@gmial.com>
2025-03-02 19:17:12 +0100

    solution_

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+NODES=("kind-worker" "kind-worker2")
 kind create cluster --config ./cluster.yml
-kubectl label nodes kind-worker kind-worker2 app=mysql
-kubectl taint nodes kind-worker kind-worker2 app=mysql:NoSchedule
+
+for NODE in "${NODES[@]}"; do
+    if kubectl get nodes | grep -q "$NODE"; then
+        echo "<<<>>> NODE '$NODE' was found. <<<>>>"
+        kubectl label nodes "$NODE" app=mysql --overwrite
+        kubectl taint nodes "$NODE" app=mysql:NoSchedule --overwrite
+    else
+        echo "NODE '$NODE' was not found."
+        exit
+    fi
+done
+
 helm dependency build ./helm-chart/todoapp/
-helm install todoapp ./helm-chart/todoapp --debug
-kubectl get pods -A -o wide
-helm list
-helm dependency list ./helm-chart/todoapp/
+helm install todoapp ./helm-chart/todoapp
+#kubectl get pods -A -o wide
+#helm list
+#helm dependency list ./helm-chart/todoapp/
